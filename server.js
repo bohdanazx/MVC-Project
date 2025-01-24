cf23e577242a931747217362d2d0a3f9d818c08e
bohdanayabl <little.elf2004@gmail.com>
2025-01-24 11:33:53 +0100

    final commit

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 //server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const readingTasksRouter = require('./routes/readingTasks');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const readingTasksRouter = require("./routes/readingTasks");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => res.render("index"));
 
-app.use('/readingTasks', readingTasksRouter);
+app.use("/readingTasks", readingTasksRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
